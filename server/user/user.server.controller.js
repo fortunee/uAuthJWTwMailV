ce7de27d3dc9d5fd2596fffbbf3e7222d506b286
Fortune Ekeruo <legalmody@gmail.com>
2017-04-24 18:43:52 +0100

    Catch errors if there are any

--- server/user/user.server.controller.js
+++ server/user/user.server.controller.js
@@ -6,6 +6,7 @@ import Auth from './../middlewares/jwtAuth';
 const secret = process.env.SECRET || 'I see you';
 
 exports.createUser = (req, res) => {
+    // hash user's password and register them
   req.body.password = pHasherMatcher.hashPassword(req.body.password);
   User.create(req.body).then((user) => {
     const token = jwt.sign({
@@ -13,12 +14,14 @@ exports.createUser = (req, res) => {
       userId: user._id,
       username: user.username
     }, secret, { expiresIn: '2 days' });
-    res.status(201).send({
+    return res.status(201).send({
       username: user.username,
       email: user.email,
       firstname: user.firstname,
       lastname: user.lastname,
       token
     });
+  }).catch((err) => {
+    res.status(400).send(err.errors);
   });
 };

5fd02095249423fc4c5bfe8d75f1b73a84807dd5
Fortune Ekeruo <legalmody@gmail.com>
2017-04-24 16:55:17 +0100

    Sign token and return a few user attributes to the user

--- server/user/user.server.controller.js
+++ server/user/user.server.controller.js
@@ -3,10 +3,22 @@ import User from './user.server.model';
 import pHasherMatcher from './../middlewares/pHasherMatcher';
 import Auth from './../middlewares/jwtAuth';
 
+const secret = process.env.SECRET || 'I see you';
+
 exports.createUser = (req, res) => {
   req.body.password = pHasherMatcher.hashPassword(req.body.password);
-  const user = req.body;
-  User.create(user).then((data) => {
-    res.status(201).send(data);
+  User.create(req.body).then((user) => {
+    const token = jwt.sign({
+      /* eslint-disable no-underscore-dangle */
+      userId: user._id,
+      username: user.username
+    }, secret, { expiresIn: '2 days' });
+    res.status(201).send({
+      username: user.username,
+      email: user.email,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      token
+    });
   });
 };
